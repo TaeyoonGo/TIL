47a1a12eaf9b56ce7e29c0793304dbb77a10b92c
Gotaeyoon <ty.go@payple.kr>
2022-07-13 22:54:42 +0900

    vue 작업환경 세팅 및 vue 이벤트 핸들러 공부

---
+++ vue/vuedongsan/src/App.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <img alt="Vue logo" src="./assets/logo.png">
+            <div class="menu">
+                <a v-for="(작명,i) in 메뉴들" :key="i">{{작명}}</a>
+            </div>
+            <div>
+                <p :style="스타일">{{logo}}</p>
+                <h4>{{products[0]}}</h4>
+                <p>{{price1}}
+                    만원</p>
+            </div>
+              <button @click="increase">허위매물신고</button><span>신고수 : {{신고수}}</span>
+            <div>
+                <h4>{{products[1]}}</h4>
+                <p>{{price2}}
+                    만원</p>
+            </div>
+            <div>
+                <h4>{{products[2]}}</h4>
+                <p>{{price2}}
+                    만원</p>
+            </div>
+        </div>
+    </template>
+
+    <script>
+
+        export default {
+            name: 'App',
+            data() {
+                return {
+                    신고수 : 0,
+                    메뉴들: [
+                        'Home', 'Shop', 'About'
+                    ],
+                    price1: 80,
+                    price2: 70,
+                    logo: '원룸샵',
+                    스타일: 'color : blue',
+                    products: ['역삼동원룸', '천호동원룸', '마포구원룸']
+                }
+            },
+
+            methods: {
+              increase(){
+                this.신고수 += 1;
+              }
+            },
+            components: {}
+        }
+    </script>
+
+    <style>
+        #app {
+            font-family: Avenir, Helvetica, Arial, sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            text-align: center;
+            color: #2c3e50;
+            margin-top: 60px;
+        }
+        .menu {
+            background: darkslateblue;
+            padding: 15px;
+            border-radius: 5px;
+        }
+        .menu a {
+            color: #fff;
+            padding: 10px;
+        }
+    </style>
